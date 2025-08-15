348c35a8b5ece452b47f00d3818e05d14ad37689
Ankith Ravindran <ankith88@gmail.com>
2025-08-15 22:03:44 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Button data-sideb

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -4,7 +4,7 @@
 import * as React from "react"
 import { Slot } from "@radix-ui/react-slot"
 import { VariantProps, cva } from "class-variance-authority"
-import { PanelLeft } from "lucide-react"
+import { PanelLeft, PanelRight } from "lucide-react"
 
 import { useIsMobile } from "@/hooks/use-mobile"
 import { cn } from "@/lib/utils"
@@ -281,7 +281,7 @@ const SidebarTrigger = React.forwardRef<
   React.ElementRef<typeof Button>,
   React.ComponentProps<typeof Button>
 >(({ className, onClick, ...props }, ref) => {
-  const { toggleSidebar } = useSidebar()
+  const { toggleSidebar, state, isMobile } = useSidebar()
 
   return (
     <Button
@@ -296,7 +296,13 @@ const SidebarTrigger = React.forwardRef<
       }}
       {...props}
     >
-      <PanelLeft />
+      {isMobile ? (
+        <PanelLeft />
+      ) : state === 'expanded' ? (
+        <PanelLeft />
+      ) : (
+        <PanelRight />
+      )}
       <span className="sr-only">Toggle Sidebar</span>
     </Button>
   )
