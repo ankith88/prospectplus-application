7e6fdaebfde10ef8d9eebd6884edb042fc3d3cd3
Ankith Ravindran <ankith88@gmail.com>
2025-08-21 20:35:49 +0000

    it is still throwing this error

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,13 +6,13 @@ import { getFirestore, Firestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "REDACTED",
-    authDomain: "REDACTED",
-    projectId: "REDACTED",
-    storageBucket: "REDACTED",
-    messagingSenderId: "REDACTED",
-    appId: "REDACTED",
-    measurementId: "REDACTED"
+    apiKey: "YOUR_API_KEY",
+    authDomain: "YOUR_AUTH_DOMAIN",
+    projectId: "YOUR_PROJECT_ID",
+    storageBucket: "YOUR_STORAGE_BUCKET",
+    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+    appId: "YOUR_APP_ID",
+    measurementId: "YOUR_MEASUREMENT_ID"
 };
 
 let app: FirebaseApp;
