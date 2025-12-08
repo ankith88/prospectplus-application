818a4db5aab4d7b9c4f5396f35b41b8f4ebab47e
Ankith Ravindran <ankith88@gmail.com>
2025-12-08 01:39:37 +0000

    The app isn't starting. Please investigate what could be wrong based on

---
+++ next.config.js
@@ -0,0 +1,37 @@
+
+const withPWA = require('@ducanh2912/next-pwa');
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'mailplus.com.au',
+        port: '',
+        pathname: '/**',
+      }
+    ],
+  },
+  serverComponentsExternalPackages: ['firebase-admin'],
+};
+
+module.exports = withPWA({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+})(nextConfig);
