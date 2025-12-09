51a432162d0c5c0f20e888bd9a4b0783147de420
Ankith Ravindran <ankith88@gmail.com>
2025-12-09 06:43:35 +0000

    on load of lead profile page, getting the below error  Server Error {na

--- src/app/leads/[id]/page.tsx
+++ src/app/leads/[id]/page.tsx
@@ -3,13 +3,28 @@ import { notFound } from 'next/navigation'
 import { getLeadFromFirebase } from '@/services/firebase'
 import { LeadProfile } from '@/components/lead-profile'
 import type { Lead } from '@/lib/types'
+import React from 'react'
 
 export default async function LeadProfilePage({
   params,
 }: {
   params: { id: string }
 }) {
+  // const resolvedParams = React.use(params);
   const { id } = params;
+
+  // params.then((resolvedData) => {
+  //   // Access the elements within 'resolvedData' here
+  //   console.log("Resolved data:", resolvedData);
+  //   // Example: If resolvedData is an object with a 'name' property
+  //   // console.log(resolvedData.name);
+  // }).catch((error) => {
+  //   // Handle any errors that occurred during Promise resolution
+  //   console.error("Error resolving Promise:", error);
+  // });
+  console.log('params: ' + params);
+  console.log('typeof params: ' + typeof params);
+  
 
   // Add a guard to ensure the ID is valid before fetching
   if (!id || typeof id !== 'string') {
