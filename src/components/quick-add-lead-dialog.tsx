1666d8b17f2d2cce01fc9ad2caf14f20c6ec07b7
Ankith Ravindran <ankith88@gmail.com>
2026-01-30 07:42:39 +0000

    also in the quick add pop-up, when the user clicks the button to open up

--- src/components/quick-add-lead-dialog.tsx
+++ src/components/quick-add-lead-dialog.tsx
@@ -113,7 +113,7 @@ export function QuickAddLeadDialog({ isOpen, onOpenChange }: QuickAddLeadDialogP
 
     const getCameraPermission = async () => {
       try {
-        const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+        const stream = await navigator.mediaDevices.getUserMedia({ video: { facingMode: 'environment' } });
         setHasCameraPermission(true);
 
         if (videoRef.current) {
