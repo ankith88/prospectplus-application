abc271dd5e6ebe4890f16d80d2caf7c7cfebadc5
Ankith Ravindran <ankith88@gmail.com>
2026-06-11 12:11:30 +1000

    chore: remove scratch files

--- scratch_stringify_ref.ts
+++
@@ -1,8 +0,0 @@
-import * as admin from 'firebase-admin';
-
-admin.initializeApp({ projectId: 'test' });
-const db = admin.firestore();
-
-const ref = db.collection('customers').doc('12345');
-console.log("Stringified DocumentReference:", JSON.stringify({ leadId: ref }));
-
